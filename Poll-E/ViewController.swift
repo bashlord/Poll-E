3fc6056cfc1a73fdcfd66fcea2c47fbbc2cfb031
Jin Woong Kim <johnjinkim1@gmail.com>
2016-10-03 22:46:35 -0700

    Initial Commit

---
+++ Poll-E/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Poll-E
+//
+//  Created by John Jin Woong Kim on 10/3/16.
+//  Copyright © 2016 John Jin Woong Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
